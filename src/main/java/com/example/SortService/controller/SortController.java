7140085b5d9728edb4a0a17612b9ffbc07376c89
strwbrry1 <matvienkosemyon@gmail.com>
2025-03-26 01:28:22 +0400

    SortController print matrix added

--- src/main/java/com/example/SortService/controller/SortController.java
+++ src/main/java/com/example/SortService/controller/SortController.java
@@ -2,19 +2,33 @@ package com.example.SortService.controller;
 
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 
 @RestController
 @RequestMapping("/api/half-sort")
 public class SortController {
+
+    int[][] halfMatrix;
+
     @PostMapping
     public ResponseEntity<int[][]> sortMatrixHalf(@RequestBody int[][] matrix) {
+        halfMatrix = matrix.clone();
         Arrays.sort(matrix, (a, b) -> Integer.compare(a[0], b[0]));
         return ResponseEntity.ok(matrix);
+    }
+
+    @GetMapping("/get-half-matrix")
+    public String printMatrix(){
+
+        String res = "";
+        for (int i = 0; i < halfMatrix.length; i++) {
+            for (int j = 0; j < halfMatrix[0].length; j++) {
+                res += halfMatrix[i][j] + " ";
+            }
+            res += "<br>";
+        }
+        return res;
     }
 }
